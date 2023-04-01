1601414746a348bb738f9e9b9f1948388569d68d
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-04-01 09:17:08 +0000

    before Test

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,61 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         Noeud n = new Noeud();
-        System.out.println(n.TestTerminaison());
-        n.setX(0,0);
-        n.setX(1,0);
-        n.setX(2,0);
-        n.Afficher();
-        System.out.println(n.TestTerminaison());
+        String[][] T = new String[3][3];
+        for (int i = 0 ; i < 3; i++)
+            for (int j = 0 ; j < 3; j++)
+                T[i][j] = "";
+        T[0][0] = "X";
+        n.setEtat(T);
+        int i = 0;
+        while(i < 5){
+            n.setEtat(MINIMAX(n));
+            i++;
+        }
+    }
+    static String[][] MINIMAX(Noeud noeud){
+
+        Noeud MeilleurCoup = new Noeud();
+
+        int maxi = (int)Double.NEGATIVE_INFINITY;
+        List<Noeud> fils = noeud.successeur("O");
+        for (Noeud f: fils) {
+            int valeur_fils = ValeurMINI(f);
+            if(valeur_fils > maxi){
+                maxi = valeur_fils;
+                MeilleurCoup.setEtat(f.getT());
+            }
+        }
+        return MeilleurCoup.getT();
+    }
+    static int ValeurMINI(Noeud noeud){
+        int utilite = noeud.utilite();
+        if(utilite != -2) return utilite;
+
+        List<Noeud> fils = noeud.successeur("O");
+
+        int v_min = (int)Double.POSITIVE_INFINITY;
+        for (Noeud f: fils) {
+            int valeur_fils = ValeurMAX(f);
+            v_min = Integer.min(v_min,valeur_fils);
+        }
+        return v_min;
+    }
+
+    static int ValeurMAX(Noeud noeud){
+        int utilite = noeud.utilite();
+        if(utilite != -2) return utilite;
+
+        List<Noeud> fils = noeud.successeur("X");
+
+        int v_max = (int)Double.NEGATIVE_INFINITY;
+        for (Noeud f: fils) {
+            int valeur_fils = ValeurMINI(f);
+            v_max = Integer.max(v_max,valeur_fils);
+        }
+        return v_max;
     }
 }
--- src/Noeud.java
+++ src/Noeud.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Noeud {
 
     private String[][] T;
@@ -9,18 +13,15 @@ public class Noeud {
                 T[i][j] = "";
     }
 
-    public void setX(int i, int j){
-        T[i][j] = "X";
-    }
-
     public String[][] getT() {
         return T;
     }
 
-    public void setEtat(String[][] tableau){
+    public Noeud setEtat(String[][] tableau){
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 T[i][j] = tableau[i][j];
+        return this;
     }
 
     public boolean TestTerminaison(){
@@ -47,14 +48,93 @@ public class Noeud {
                 return true;
             }
         }
+        if (Remplie()){
+            System.out.println("Egalité");
+            return true;
+        }
         return false;
     }
 
-    public int utilite(){
-        int value = 0;
-        
-        return value;
+    public List<Noeud> successeur(String str){
+        List<Noeud> fils = new ArrayList<>();
+
+        Noeud n = new Noeud();
+        n.setEtat(this.getT());
+
+        //premier fils
+        outerloop:
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (n.T[i][j] == "") {
+                    n.T[i][j] = str;
+                    fils.add(n);
+                    break outerloop;
+                }
+            }
+        }
+
+        for (int k = 0 ; k < 9; k++){
+            outerloop:
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++) {
+                    if (n.T[i][j] == ""){
+                        n.T[i][j] = str;
+
+                        Noeud child = new Noeud();
+                        child.setEtat(this.getT());
+                        child.T[i][j] = str;
+
+                        int check = 0;
+                        for (Noeud f: fils) {
+                            if (stringEqual(f.getT(),child.getT()))
+                                check =1;
+                        }
+                        if (check == 0) fils.add((new Noeud()).setEtat(child.getT()));
+
+                        break outerloop;
+                    }
+                }
+            }
+        }
+        return fils;
     }
+    public boolean Remplie(){
+        for (int i = 0 ; i < 3; i++)
+            for (int j = 0 ; j < 3; j++)
+                if(T[i][j] == null)
+                    return false;
+        return true;
+    }
+
+    public int utilite(){
+        // diagonal
+        if(T[0][0] == T[1][1] && T[1][1] == T[2][2] && T[0][0] != ""){
+            if(T[0][0] == "X") return 1;
+            return -1;
+        }
+        if(T[0][2] == T[1][1] && T[1][1] == T[2][0] && T[1][1] != ""){
+            if(T[0][2] == "X") return 1;
+            return -1;
+        }
+        // lignes
+        for (int i = 0; i < 3; i++){
+            if(T[i][0] == T[i][1] && T[i][1] == T[i][2] && T[i][2] != ""){
+                if(T[i][0] == "X") return 1;
+                return -1;
+            }
+        }
+        // collones
+        for (int i = 0; i < 3; i++){
+            if(T[0][i] == T[1][i] && T[1][i] == T[2][i] && T[2][i] != ""){
+                if(T[0][i] == "X") return 1;
+                return -1;
+            }
+        }
+        if(Remplie()) return 0;
+        // feuille
+        return -2;
+    }
+
     public void Afficher(){
         for (int i = 0 ; i < 3; i++){
             System.out.println("------------");
@@ -64,8 +144,25 @@ public class Noeud {
             System.out.println();
         }
     }
+
     @Override
     public String toString() {
         return ""+T[0][0];
+    }
+
+    private boolean stringEqual(String[][] array1, String[][] array2){
+        boolean equal = true;
+
+        if (array1.length != array2.length) {
+            equal = false;
+        } else {
+            for (int i = 0; i < array1.length; i++) {
+                if (!Arrays.equals(array1[i], array2[i])) {
+                    equal = false;
+                    break;
+                }
+            }
+        }
+        return equal;
     }
 }
